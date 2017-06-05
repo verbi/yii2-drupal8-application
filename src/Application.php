cf98c2743d034900137aee29d429856a842c2dc7
Philip Verbist <philip.verbist@gmail.com>
2017-06-05 23:08:23 +0200

    initial commit

---
+++ src/Application.php
@@ -0,0 +1,92 @@
+<?php
+
+
+namespace verbi\yii2Drupal8Application;
+use Yii;
+use yii\helpers\Url;
+use yii\base\InvalidRouteException;
+
+/**
+ * Application is the base class for all drupal8 application classes.
+ *
+ *
+ * @property ErrorHandler $errorHandler The error handler application component. This property is read-only.
+ * @property string $homeUrl The homepage URL.
+ * @property Request $request The request component. This property is read-only.
+ * @property Response $response The response component. This property is read-only.
+ * @property Session $session The session component. This property is read-only.
+ * @property User $user The user component. This property is read-only.
+ *
+ * @author Philip Verbist <philip.verbist@gmail.com>
+ * @since 2.0
+ */
+class Application extends \yii\web\Application
+{
+    /**
+     * @inheritdoc
+     */
+    protected function bootstrap()
+    {
+        $request = $this->getRequest();
+        Yii::setAlias('@webroot', dirname($request->getScriptFile()));
+        Yii::setAlias('@web', $request->getBaseUrl());
+        parent::bootstrap();
+    }
+    /**
+     * Handles the specified request.
+     * @param Request $request the request to be handled
+     * @return Response the resulting response
+     * @throws NotFoundHttpException if the requested route is invalid
+     */
+    public function handleRequest($request)
+    {
+        if (empty($this->catchAll)) {
+            try {
+                list ($route, $params) = $request->resolve();
+            } catch (UrlNormalizerRedirectException $e) {
+                $url = $e->url;
+                if (is_array($url)) {
+                    if (isset($url[0])) {
+                        // ensure the route is absolute
+                        $url[0] = '/' . ltrim($url[0], '/');
+                    }
+                    $url += $request->getQueryParams();
+                }
+                return $this->getResponse()->redirect(Url::to($url, $e->scheme), $e->statusCode);
+            }
+        } else {
+            $route = $this->catchAll[0];
+            $params = $this->catchAll;
+            unset($params[0]);
+        }
+        try {
+            Yii::trace("Route requested: '$route'", __METHOD__);
+            $this->requestedRoute = $route;
+            $result = $this->runAction($route, $params);
+            if ($result instanceof Response) {
+                return $result;
+            } else {
+                $response = $this->getResponse();
+                if ($result !== null) {
+                    $response->data = $result;
+                }
+                return $response;
+            }
+        } catch (InvalidRouteException $e) {
+            throw new NotFoundHttpException(Yii::t('yii', 'Page not found.'), $e->getCode(), $e);
+        }
+    }
+    /**
+     * @inheritdoc
+     */
+    public function coreComponents()
+    {
+        return array_merge(parent::coreComponents(), [
+            'request' => ['class' => 'yii\web\Request'],
+            'response' => ['class' => 'yii\web\Response'],
+            'session' => ['class' => 'yii\web\Session'],
+            'user' => ['class' => 'yii\web\User'],
+            'errorHandler' => ['class' => 'yii\web\ErrorHandler'],
+        ]);
+    }
+}
